de5e5493d4d4e26b2e6eccb0870c85fbc0283b19
Swen <swenmeeu@gmail.com>
2018-12-29 16:15:45 +0100

    The `uid` field is now a string and unique in Sensor Model

--- src/models/sensor.model.js
+++ src/models/sensor.model.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const uniqueValidator = require('mongoose-unique-validator');
 
 const SensorSchema = new Schema({
-    uid: { type: Number, required: true },
+    uid: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     type: { type: String, required: true },
     location: { type: String, required: true }
 });
+
+SensorSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Sensor', SensorSchema);
