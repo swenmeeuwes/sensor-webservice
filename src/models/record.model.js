0cc463cf3977e4a664ad84144bf7794d418a69b9
Swen <swenmeeu@gmail.com>
2018-12-29 16:36:35 +0100

    Auto formatting

--- src/models/record.model.js
+++ src/models/record.model.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const RecordSchema = new Schema({
-    timestamp: { type: String, required: true },
-    sensor: { type: String, required: true },
-    unit: { type: String, required: true },
-    value: { type: Number, required: true }
+  timestamp: { type: String, required: true },
+  sensor: { type: String, required: true },
+  unit: { type: String, required: true },
+  value: { type: Number, required: true }
 });
 
 module.exports = mongoose.model('Record', RecordSchema);
--- src/models/sensor.model.js
+++ src/models/sensor.model.js
@@ -3,10 +3,10 @@ const Schema = mongoose.Schema;
 const uniqueValidator = require('mongoose-unique-validator');
 
 const SensorSchema = new Schema({
-    uid: { type: String, required: true, unique: true },
-    name: { type: String, required: true },
-    type: { type: String, required: true },
-    location: { type: String, required: true }
+  uid: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  type: { type: String, required: true },
+  location: { type: String, required: true }
 });
 
 SensorSchema.plugin(uniqueValidator);
