67ed29c03c17a87bfcecc697d1f0d3cf8b042d09
Swen <swenmeeu@gmail.com>
2018-12-29 16:15:13 +0100

    Updated api doc comments

--- src/routes/sensor.route.js
+++ src/routes/sensor.route.js
@@ -4,30 +4,119 @@ const router = express.Router();
 const sensorController = require('../controllers/sensor.controller');
 
 /**
- * @api {post} /create Request User information
- * @apiName GetUser
- * @apiGroup User
+ * @api {post} /create Create Sensor
+ * @apiVersion 1.0.0
+ * @apiName CreateSensor
+ * @apiGroup Sensor
  *
- * @apiParam {Number} uid Sensors unique ID.
- * @apiParam {Name} name Sensors name.
- * @apiParam {Type} type Sensors type.
- * @apiParam {Location} location Sensors location.
+ * @apiParam {String} uid Sensors unique ID.
+ * @apiParam {String} name Sensors name.
+ * @apiParam {String} type Sensors type.
+ * @apiParam {String} location Sensors location.
+ * 
+ * @apiSuccess {String} uid Sensors unique ID.
+ * @apiSuccess {String} name Sensors name.
+ * @apiSuccess {String} type Sensors type.
+ * @apiSuccess {String} location Sensors location.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "uid": "73DKQ9IU",
+ *          "name": "Living Room",
+ *          "type": "Temperature",
+ *          "Location": "Living Room"
+ *     }
+ */
+router.post('/create', sensorController.create);
+
+/**
+ * @api {get} /:uid Sensor Details
+ * @apiVersion 1.0.0
+ * @apiName GetSensor
+ * @apiGroup Sensor
+ *
+ * @apiSuccess {String} uid Sensors unique ID.
+ * @apiSuccess {String} name Sensors name.
+ * @apiSuccess {String} type Sensors type.
+ * @apiSuccess {String} location Sensors location.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "uid": "73DKA9EU",
+ *          "name": "Living Room",
+ *          "type": "Temperature",
+ *          "Location": "Living Room"
+ *     }
+ */
+router.get('/:uid', sensorController.details);
+
+/**
+ * @api {put} /:uid Update Sensor
+ * @apiVersion 1.0.0
+ * @apiName UpdateSensor
+ * @apiGroup Sensor
+ *
+ * @apiParam {String} uid Sensors unique ID.
+ * @apiParam {String} name Sensors name.
+ * @apiParam {String} type Sensors type.
+ * @apiParam {String} location Sensors location.
+ * 
+ * @apiSuccess {String} uid Sensors unique ID.
+ * @apiSuccess {String} name Sensors name.
+ * @apiSuccess {String} type Sensors type.
+ * @apiSuccess {String} location Sensors location.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "uid": "73DKA9QU",
+ *          "name": "Living Room",
+ *          "type": "Humidity",
+ *          "Location": "Living Room"
+ *     }
+ */
+router.put('/:uid', sensorController.update);
+
+/**
+ * @api {patch} /:uid Update Sensor
+ * @apiVersion 1.0.0
+ * @apiName UpdateSensor
+ * @apiGroup Sensor
+ *
+ * @apiParam {String} uid Sensors unique ID.
+ * @apiParam {String} name Sensors name.
+ * @apiParam {String} type Sensors type.
+ * @apiParam {String} location Sensors location.
+ * 
+ * @apiSuccess {String} uid Sensors unique ID.
+ * @apiSuccess {String} name Sensors name.
+ * @apiSuccess {String} type Sensors type.
+ * @apiSuccess {String} location Sensors location.
+ *
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ *     {
+ *          "uid": "73DKA9QU",
+ *          "name": "Living Room",
+ *          "type": "Humidity",
+ *          "Location": "Living Room"
+ *     }
+ */
+router.patch('/:uid', sensorController.update);
+
+/**
+ * @api {delete} /:uid Delete Sensor
+ * @apiVersion 1.0.0
+ * @apiName DeleteSensor
+ * @apiGroup Sensor
  *
  * @apiSuccessExample Success-Response:
  *     HTTP/1.1 200 OK
  *     {
  *     }
- *
- * @apiErrorExample Error-Response:
- *     HTTP/1.1 400 Bad Request
- *     {
- * 
- *     }
  */
-router.post('/create', sensorController.create);
-router.get('/:uid', sensorController.details);
-router.put('/:uid', sensorController.update);
-router.patch('/:uid', sensorController.update);
 router.delete('/:uid', sensorController.delete);
 
 module.exports = router;
