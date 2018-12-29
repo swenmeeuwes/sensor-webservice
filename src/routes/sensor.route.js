54aa1c5f521b718e16a5985f16b49bb1d1c57f74
Swen <swenmeeu@gmail.com>
2018-12-29 16:36:23 +0100

    Updated sensor route api doc comments

--- src/routes/sensor.route.js
+++ src/routes/sensor.route.js
@@ -13,19 +13,10 @@ const sensorController = require('../controllers/sensor.controller');
  * @apiParam {String} name Sensors name.
  * @apiParam {String} type Sensors type.
  * @apiParam {String} location Sensors location.
- * 
- * @apiSuccess {String} uid Sensors unique ID.
- * @apiSuccess {String} name Sensors name.
- * @apiSuccess {String} type Sensors type.
- * @apiSuccess {String} location Sensors location.
  *
  * @apiSuccessExample Success-Response:
- *     HTTP/1.1 200 OK
+ *     HTTP/1.1 201 OK
  *     {
- *          "uid": "73DKQ9IU",
- *          "name": "Living Room",
- *          "type": "Temperature",
- *          "Location": "Living Room"
  *     }
  */
 router.post('/create', sensorController.create);
