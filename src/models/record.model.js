fa739c3ed2f7f0c910632484cd3369d2b3797b6b
Swen <swenmeeu@gmail.com>
2018-12-29 16:16:11 +0100

    Implemented record model

---
+++ src/models/record.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const RecordSchema = new Schema({
+    timestamp: { type: String, required: true },
+    sensor: { type: String, required: true },
+    unit: { type: String, required: true },
+    value: { type: Number, required: true }
+});
+
+module.exports = mongoose.model('Record', RecordSchema);
